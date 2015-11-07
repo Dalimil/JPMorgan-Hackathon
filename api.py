2ea5ad66b05aa6dedd476e14bba5e4f840f9c97d
seuqaj114 <migjacques@hotmail.com>
2015-11-07 18:10:03 +0000

    Added debug.

--- api.py
+++ api.py
@@ -7,7 +7,9 @@ api = Blueprint('api', __name__, template_folder='templates')
 @api.route("/authenticate", methods=['POST'])
 def authenticate():
 	user = json.loads(request.data)
+	print user
 	q = User.query.filter_by(email=user["email"], password=user["password"]).first()
+	print q
 	if q:
 		return json.dumps({"result":True})
 	else:
