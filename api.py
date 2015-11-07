aa85f79485024c4cb4e35461a34d943a2b2a0adf
KamenB <kbrest@abv.bg>
2015-11-07 18:10:57 +0000

    Remove print in api

--- api.py
+++ api.py
@@ -7,7 +7,6 @@ api = Blueprint('api', __name__, template_folder='templates')
 @api.route("/authenticate", methods=['POST'])
 def authenticate():
 	user = json.loads(request.data)
-	print "AA"
 	q = User.query.filter_by(email=user["email"], password=user["password"]).first()
 	if q:
 		return json.dumps({"result":True})
