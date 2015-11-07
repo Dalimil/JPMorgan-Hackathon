78d5e65699ed03c976241f36bcc1aea0b5db6668
seuqaj114 <migjacques@hotmail.com>
2015-11-07 17:57:20 +0000

    Changed DB.

--- models.py
+++ models.py
@@ -2,7 +2,14 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+user_identifier = db.Table('user_identifier',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('project_id', db.Integer, db.ForeignKey('projects.id'))
+)
+
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
@@ -18,3 +25,10 @@ class User(db.Model):
     def __str__(self):
         return "{0} {1} {2} {3}".format(self.first_name, self.last_name, self.email, self.password)
 
+class Project(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    img = db.Column(db.Text)
+    users = db.relationship("User",secondary=user_identifier)
