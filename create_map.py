9bb6eae596b7286d3973a247bee35133793ec607
KamenB <kbrest@abv.bg>
2015-11-07 22:38:03 +0000

    Added comments to create_map

--- create_map.py
+++ create_map.py
@@ -6,6 +6,9 @@ def create_map(style, markers, info_boxes):
         style=style,
         zoom="16",
         maptype="TERRAIN",
+        # Infobox is a list of a sequence of html tags. Example ["<img src='./static/a.jpg' height = 100 width = 100>", "<span><p>Text</p></span>"]
+        # Just plain text does not display in infoboxes. It has to be put into a tag.
         infobox=info_boxes,
+        # Markers is a list of tuples of coordinates. Example: [(45.223, 92.114)]
         markers=markers)
     return mymap
