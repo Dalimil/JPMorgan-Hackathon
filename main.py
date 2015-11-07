75896303ee0780321b0290b91975571a34d957a3
Dalimil <dalimilhajek@gmail.com>
2015-11-07 14:54:33 +0000

    Update endpoints

--- main.py
+++ main.py
@@ -7,25 +7,60 @@ app.secret_key = "bnNoqxXSgzoXSOezxpfdvadrMp5L0L4mJ4o8nRzn"
 
 @app.route('/')
 def index():
+	if 'username' in session:
+		# loggedIn
+		username = session['username']
+        print('Logged in as {}'.format(name))
+
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		passw = request.form['password']
+		# check credentials against database here
+		valid = True
+		if(valid):
+			return redirect(url_for('index'))
+	
     return render_template('login.html')
 
 @app.route('/register', methods=['POST'])
 	#handle new user registration here
-	
-	return redirect(url_for('login'))
+
+	# get form data
+	# name = request.form['name']
+	# ...
+
+	#save database
+
+	# log them in
+	#session['username'] = request.form['username']
+
+	# redirect
+	return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
 	# close session and redirect to index
-    return "logout"
+	session.pop('username', None)
+    return redirect(url_for('index'))
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 def admin():
-	return render_template('admin.html')
+	loggedIn = False
+	if request.method == 'POST':
+		username = request.form['username']
+		passw = request.form['password']
+		if(username=='admin' && 
+	else:
+		if 'admin' in session:
+			#logged in as admin
+			loggedIn = True
+
+
+	return render_template('admin.html', loggedIn=loggedIn)
 
 @app.route('/report')
 def report():
