7d06c81115436cdc1dfc3702b56adcdd7b24a22a
KamenB <kbrest@abv.bg>
2015-11-07 22:32:35 +0000

    CreateMap fixed

--- create_map.py
+++ create_map.py
@@ -1,17 +1,11 @@
-def create_map(post_code, style):
-	r = requests.post(
-		url="http://maps.googleapis.com/maps/api/geocode/json?address=" + post_code, 
-		data=json.dumps({})
-	)
-	coordinates_object = json.loads(r.text)["results"][0]["geometry"]["bounds"]["northeast"]
-
-	mymap = Map(
-    	identifier="view-side",
-    	lat=coordinates[0],
-    	lng=coordinates[1],
-    	style=style,
-    	zoom="15",
-    	maptype="TERRAIN",
-    	infobox=["<img src='http://www.keenthemes.com/preview/conquer/assets/plugins/jcrop/demos/demo_files/image1.jpg' height=100 width=100>"],
-    	markers=[coordinates])
-	return mymap
+def create_map(style, markers, info_boxes):
+    mymap = Map(
+        identifier="view-side",
+        lat=55.855478,
+        lng=-4.2560766,
+        style=style,
+        zoom="16",
+        maptype="TERRAIN",
+        infobox=info_boxes,
+        markers=markers)
+    return mymap
