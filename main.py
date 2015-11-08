3cd4279fe044c732633fe858442a012849169114
Dalimil <dalimilhajek@gmail.com>
2015-11-08 03:12:30 +0000

    Change admin page

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ def index():
         names_my_p = [i["name"] for i in my_projects]
         greens = [(i["lat"], i["lng"]) for i in all_projects if i["name"] in names_my_p]
         reds = [(i["lat"], i["lng"]) for i in all_projects if i["name"] not in names_my_p]
-        infoboxReds = ["<p>"+i["name"][:1].upper()+i["name"][1:]+"</p>" for i in all_projects if i["name"] not in names_my_p]
-        infoboxGreens = ["<p>"+i["name"][:1].upper()+i["name"][1:]+"</p>" for i in all_projects if i["name"] in names_my_p]
+        infoboxReds = ["<p><strong>"+i["name"][:1].upper()+i["name"][1:]+"</strong></p><p><strong>Availability: </strong>"+str(i["count"])+"/"+str(i["num_people"])+"</p>" for i in all_projects if i["name"] not in names_my_p]
+        infoboxGreens = ["<p><strong>"+i["name"][:1].upper()+i["name"][1:]+"</strong></p><p><strong>Availability: </strong>"+str(i["count"])+"/"+str(i["num_people"])+"</p>" for i in all_projects if i["name"] in names_my_p]
         
         all_projects_map = create_map("width:100%;height:400px;border: 1px solid black; border-radius: 15px;", {"http://maps.google.com/mapfiles/ms/icons/green-dot.png":greens, "http://maps.google.com/mapfiles/ms/icons/red-dot.png":reds}, infoboxGreens+infoboxReds)
         all_projects = [i for i in all_projects if i["name"] not in names_my_p]
@@ -119,24 +119,9 @@ def admin():
             loggedIn = True
             all_projects = json.loads(requests.get(url="https://5812d998.ngrok.com/projects").text)["data"]
             all_users = json.loads(requests.get(url="https://5812d998.ngrok.com/users").text)["data"]
+            all_projects_map = create_map("width:100%;height:400px;border: 1px solid black; border-radius: 15px;", [(i["lat"], i["lng"]) for i in all_projects], ["<p><strong>"+i["name"][:1].upper()+i["name"][1:]+"</strong></p><p><strong>Availability: </strong>"+str(i["count"])+"/"+str(i["num_people"])+"</p>" for i in all_projects])
 
     return render_template('admin.html', loggedIn=loggedIn, all_projects=all_projects)
-
-@app.route('/admin/volunteer', methods=['GET', 'POST'])
-def volunteer():
-    return render_template('volunteer.html')
-    if 'admin' in session:
-        if request.method == 'POST':
-            firstname = request.form['firstname']
-            lastname = request.form['lastname']
-            email = request.form['email']
-            postcode = request.form['postcode']
-            interests = request.form['interests']
-             
-        return render_template('volunteer.html');
- 
-    else:
-        return redirect(url_for('admin'))
 
 
 @app.route('/admin/projects',methods=['GET','POST'])
